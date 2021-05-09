1dce876c69384346d74551d849588e7a50e9b86f
ujjwal <yenahtag.uj@gmail.com>
2021-05-09 11:32:57 +0530

    About and pollicies

---
+++ LocalConnWeb/Areas/Admin/Models/utblPolicyPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LocalConnWeb.Areas.Admin.Models
+{
+    public class utblPolicyPoint
+    {
+        [Key]
+        public long PolicyPointID { get; set; }
+        public long PolicyID { get; set; }
+        public string PolicyPoints { get; set; }
+    }
+}
