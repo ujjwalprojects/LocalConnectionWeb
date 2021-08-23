ed8643d74b07574e922b559315e7e0ee39154ea5
frisbee77 <siddharth.chettri@yahoo.com>
2021-08-23 20:15:39 +0530

    new commit 20 aug

---
+++ LocalConnWeb/ViewModels/HomePageVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocalConnWeb.ViewModels
+{
+    public class HomePageVM
+    {
+        public List<CityList> cityLists { get; set; }
+        public List<HotelList> hotelList { get; set; }
+        public List<HotelList> resortList { get; set; }
+        public List<HotelList> lodgeList { get; set; }
+        public List<HotelList> guestHouseList { get; set; }
+        public List<HotelList> homestayList { get; set; }
+        public List<FtHotelList> FHotelList { get; set; }
+    }
+}
